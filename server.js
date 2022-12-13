cab51d9f3a129ded7327c77628d9f81501526a26
sming0305 <108119110+sming0305@users.noreply.github.com>
2022-12-13 13:20:08 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
+const auth = require("json-server-auth");
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
+server.use(auth);
 server.use(router);
 
 server.listen(port);
